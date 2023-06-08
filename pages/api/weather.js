40ddd7d76b62caf7345d4b415868f9eed8b577c6
Iverick <iverick091@gmail.com>
2023-06-09 02:00:41 +0300

    Add GET request to the weatherapi.com

---
+++ pages/api/weather.js
@@ -0,0 +1,15 @@
+import axios from "axios"
+
+// http://api.weatherapi.com/v1/forecast.json
+export default async (req, res) => {
+  try {
+    let city = "Dublin"
+    const weather = await axios.get(`http://api.weatherapi.com/v1/forecast.json?key=${process.env.WEATHER_API_KEY}&q=${city}&days=10`)  
+    const data = weather.data
+    return res.status(200).json({ message: "Sent successfully", mainData: data })
+  } 
+  catch(err) {
+    console.log(err)
+    return res.status(500).json({ message: "Internal server error" })
+  }
+}
