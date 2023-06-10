fdd0590e529b6f5085c92d58b5cdda595dc6c955
Iverick <iverick091@gmail.com>
2023-06-10 20:49:11 +0300

    Add toggler between imperial and metric units

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,7 @@ export default function Home() {
     name: "", weather: "", temp: "", pressure: "", humidity: "", wind: "", date: "", temp_f: "", wind_mile: ""
   })
   const [weatherForecastArray, setWeatherForecastArray] = useState([])
+  const [imperial, setImperial] = useState(false)
 
   const fetchWeather = async () => {
     try {
@@ -34,6 +35,10 @@ export default function Home() {
     }
   }
 
+  const handleImperialCheckbox = () => {
+    setImperial(!imperial)
+  }
+
   return (
     <div className="bg-blue-50 mx-20 mt-20">
       <div className="flex items-center justify-center m-4">
@@ -50,13 +55,36 @@ export default function Home() {
           Search
         </button>
       </div>
+
+      <label className="relative inline-flex items-center cursor-pointer m-4">
+        <input type="checkbox" checked={imperial} onChange={handleImperialCheckbox} className="sr-only peer" />
+        <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>      
+        <span className="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">
+          { imperial ? `Imperial Units` : `Metric Units` }
+        </span>
+      </label>
+
       <div className="bg-amber-50 p-10">
         <h1 className="text-xl">Your city: <span className="font-bold m-2">{ currentCity.name }</span></h1>
         <h3 className="text-sm my-2">Weather: <span className="font-bold m-2">{ currentCity.weather }</span></h3>
-        <h3 className="text-sm my-2">Temperature: <span className="font-bold m-2">{ currentCity.temp }</span></h3>
+        <h3 className="text-sm my-2">Temperature: 
+          <span className="font-bold m-2">
+            { imperial ? (
+              <span className="font-bold m-2">{currentCity.temp_f} {currentCity.temp && <span>F</span>}</span>
+            ) : (
+              <span className="font-bold m-2">{currentCity.temp} {currentCity.temp && <span>C</span>}</span>
+            )}
+          </span>
+        </h3>
         <h3 className="text-sm my-2">Pressure: <span className="font-bold m-2">{ currentCity.pressure }</span></h3>
         <h3 className="text-sm my-2">Humidity: <span className="font-bold m-2">{ currentCity.humidity }</span></h3>
-        <h3 className="text-sm my-2">Wind: <span className="font-bold m-2">{ currentCity.wind }</span></h3>
+        <h3 className="text-sm my-2">Wind: 
+          { imperial ? (
+            <span className="font-bold m-2">{currentCity.wind_mile} {currentCity.wind_mile && <span>m/h</span>}</span>
+          ) : (
+            <span className="font-bold m-2">{currentCity.wind} { currentCity.wind && <span>km/h</span>}</span>
+          )}
+        </h3>
         <h3 className="text-sm my-2">Date: <span className="font-bold m-2">{ currentCity.date }</span></h3>
       </div>
       <div className="m-6 font-bold">Weather Forecast: </div>
@@ -72,10 +100,22 @@ export default function Home() {
                   Weather: <span className="font-bold m-2">{weatherForecastArray[index].day.condition.text}</span>
                 </h4>
                 <h4>
-                  Min: <span className="font-bold m-2">{weatherForecastArray[index].day.mintemp_c}</span>
+                  Min: <span className="font-bold m-2">
+                    { imperial ? (
+                      <span className="font-bold m-2">{weatherForecastArray[index].day.mintemp_f} F</span>
+                    ) : (
+                      <span className="font-bold m-2">{weatherForecastArray[index].day.mintemp_c} C</span>
+                    )}
+                  </span>
                 </h4>
                 <h4>
-                  Max: <span className="font-bold m-2">{weatherForecastArray[index].day.maxtemp_c}</span>
+                  Max: <span className="font-bold m-2">
+                    { imperial ? (
+                      <span className="font-bold m-2">{weatherForecastArray[index].day.maxtemp_f} F</span>
+                    ) : (
+                      <span className="font-bold m-2">{weatherForecastArray[index].day.maxtemp_c} C</span>
+                    )}
+                  </span>
                 </h4>
               </div>
             )
